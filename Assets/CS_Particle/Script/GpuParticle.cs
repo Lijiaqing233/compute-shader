a310314cb2ae8ddfabfa6114750a28173ce75b04
lijiaqing <ri_kakei@yahoo.co.jp>
2021-03-12 21:41:57 +0900

    1

--- Assets/CS_Particle/Script/GpuParticle.cs
+++ Assets/CS_Particle/Script/GpuParticle.cs
@@ -14,10 +14,8 @@ public class GpuParticle : MonoBehaviour{
     int blockPerGrid;
     ComputeBuffer ParticleBuffer,argsBuffer;
     private uint[] _args;
-    private float time;
-    private double dt = 0.01;
     public bool use_GPU = true;
-    static public int N = 16384;
+    static public int N = 4000;   // 16384
     [Range(0, 1)]
     public float lerpt;
     [Range(0.001f, 0.008f)]
@@ -37,15 +35,14 @@ public class GpuParticle : MonoBehaviour{
       
         for (int i = 0; i < N; i++)
         {
-               double x = Random.value * 2 - 1;       
-               double y = Random.value * 2 - 1;
-            double x_v = Random.value * 2 - 1;
-            double y_v = Random.value * 2 - 1;
-            particles[i].position = new Vector3((float)x, (float)y, 0);
+            double x = Random.value * 2 - 1;       
+            double y = Random.value * 2 - 1;
+            double z = Random.value * 2 - 1;
+            particles[i].position = new Vector3((float)x, (float)y, (float)z);
             particles[i].v = new Vector3(0, 0, 0);
             //   particles[i].v = new Vector3((float)x_v, (float)y_v, 0);
             particles[i].a = new Vector3(0, 0, 0);
-                particles[i].color = new Vector3(1, 1, 1);
+                particles[i].color = new Vector3(1, (float)0.21, (float)0.31);
         }
 
         ComputeBuffer computeBuffer = new ComputeBuffer(N, 48);
@@ -54,31 +51,18 @@ public class GpuParticle : MonoBehaviour{
 
         _args = new uint[5] { 0, 0, 0, 0, 0 };
         argsBuffer = new ComputeBuffer(1, _args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
-        time = 4f;
         blockPerGrid = (N + ThreadBlockSize - 1) / ThreadBlockSize;
         
     }
 
     
     void Update(){
-        time -= Time.deltaTime;
-
         //transform.Rotate(new Vector3(0f, Time.deltaTime * 10f, 0f));
-        if (use_GPU == true)
-        {
+
             updatebuffer();
             argsBuffer.SetData(_args);
             Graphics.DrawMeshInstancedIndirect(Particle_Mesh, 0, _material, new Bounds(Vector3.zero, new Vector3(100f, 100f, 100f)), argsBuffer);
-        }
-        else
-        {
-            cpu_updatebuffer();
-            for (int i = 0; i < N; i++)
-            {
-                Graphics.DrawMesh(Particle_Mesh, particles[i].position, Quaternion.identity, _material, 0);
-
-            }
-        }
+        
 
     }   
 
@@ -100,37 +84,5 @@ public class GpuParticle : MonoBehaviour{
         _material.SetFloat("_Size", size);
        
     }
-
-
-    void cpu_updatebuffer() {
-        Vector3 r;
-        double soften = 1e-6;
-        float dist2, dist6, invDist3, s;
-
-        for (int i = 0; i < N; i++)
-            particles[i].a = Vector3.zero;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                r = particles[j].position - particles[i].position;
-                dist2 = r.x * r.x + r.y * r.y + (float)soften;
-                dist6 = dist2 * dist2 * dist2;
-                invDist3 = (float)1.0 / Mathf.Sqrt(dist6);
-
-                particles[i].a = particles[i].a + r * invDist3;
-
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            particles[i].position = particles[i].position + (float)dt * particles[i].v;
-            particles[i].v = particles[i].v + (float)dt * particles[i].a;
-
-        }
-
-    }
-
-
-
 
 }
